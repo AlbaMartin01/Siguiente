dcb877dabc33673a7ef541df11b3867afcd78a30
AlbaMartin01 <albmar891@alu.centroafuera.es>
2023-01-31 09:56:53 +0100

    Diseño pagina principal sin funcionamiento de los tres botones

--- app/src/main/java/com/example/siguiente/MainActivity.kt
+++ app/src/main/java/com/example/siguiente/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.siguiente
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<ImageView>(R.id.imagenUsuario).setBackgroundDrawable(null);
     }
 }
