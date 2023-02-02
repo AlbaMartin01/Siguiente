af0d7cefd430bd9606d16098bfb114a73280b180
AlbaMartin01 <albmar891@alu.centroafuera.es>
2023-02-02 12:57:00 +0100

    Diseño y funcionamiento (kotlin) terminado completamente

--- app/src/main/java/com/example/siguiente/MainActivity.kt
+++ app/src/main/java/com/example/siguiente/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.siguiente
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<ImageView>(R.id.imagenUsuario).setBackgroundDrawable(null);
+
+        findViewById<Button>(R.id.botonMaps).setOnClickListener {
+            var intento = Intent(this, Maps::class.java)
+            startActivity(intento)
+        }
+
+        findViewById<Button>(R.id.botonCall).setOnClickListener {
+            var intento = Intent(this, Call::class.java)
+            startActivity(intento)
+        }
+
+        findViewById<Button>(R.id.botonPlay).setOnClickListener {
+            var intento = Intent(this, Play::class.java)
+            startActivity(intento)
+        }
+
     }
 }
